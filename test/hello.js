485f0f336acdaddc4783747b063db86753524802
upeka <upekawarusahennadi@gmail.com>
2026-03-02 09:22:35 +0530

    3rd changing

--- test/hello.js
+++ test/hello.js
@@ -1,2 +1,12 @@
-console.log('Hello, world!');
-console.log('hi im upeka');
+// let studentname = ["upeka", "John", "Jane", "Doe"];
+
+// console.log("Hello " + studentname[0] + "!");
+// console.log("Hello " + studentname[1] + "!");
+// console.log("Hello " + studentname[2] + "!");
+
+function squre(num1,num2) {
+    return num1*num2;
+
+}
+
+console.log(squre(5, 5));
---
+++ test/objects_classes.js
@@ -0,0 +1,7 @@
+const user1 ={
+    name: "John",
+    id: 1,
+    password: "password123",
+}
+
+console.log(user1);
